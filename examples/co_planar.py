08031bea24bcd58e245c6128ce364b34d7abacbd
GlennWSo <sondellglenn@gmail.com>
2023-03-01 09:31:34 +0100

    colors

--- examples/co_planar.py
+++ examples/co_planar.py
@@ -17,6 +17,7 @@ cube2 = pv.Cube((1.0, 1.0, 1.0), x_length=2.0, y_length=2.0, z_length=2.0)
 p = pv.Plotter()
 p.add_mesh(cube1, color="blue", opacity=0.5)
 p.add_mesh(cube2, color="green", opacity=0.5)
+p.show_axes()
 p.show()
 
-diff(cube2, cube1).plot(text="diff")
+diff(cube2, cube1).plot(text="diff", color="yellow")
