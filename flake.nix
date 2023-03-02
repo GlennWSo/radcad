2929f426d2909e7f42675b6b231111a48b8f93f7
GlennWSo <sondellglenn@gmail.com>
2023-03-02 13:18:26 +0100

    orginize deps

--- flake.nix
+++ flake.nix
@@ -21,21 +21,35 @@
         py = pkgs.python310Packages;
         pv = pyvista.packages.${system}.pyvista;
 
+        runTime = [
+          pv
+          py.python
+          pkgs.glibc
+        ];
+        buildTime = [
+          py.twine
+          py.build
+          py.setuptools
+          py.setuptools-rust
+          pkgs.rustPlatform.rust.cargo
+          pkgs.rustPlatform.rust.rustc
+        ];
+        devTools = [
+          pkgs.rust-analyzer
+          pkgs.nil
+          py.twine
+          py.build
+        ];
+        
+    
+
       in
         {
           devShell = pkgs.mkShell rec {
             name = "pyrust";
             venvDir = ".venv";
 
-            buildInputs = [
-              pkgs.nil
-              py.python
-              pv
-              pkgs.glibc
-              pkgs.rustPlatform.rust.cargo
-              pkgs.rustPlatform.rust.rustc
-              pkgs.rust-analyzer
-            ];
+            buildInputs = runTime ++ buildTime ++ devTools;
 
             shellHook = ''
               SOURCE_DATE_EPOCH=$(date +%s)
