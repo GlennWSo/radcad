cdec6e6b1bbcefcf2a859b736978eeab7f295fb3
GlennWSo <sondellglenn@gmail.com>
2023-03-08 14:21:37 +0100

    dont close by coordinates

--- concave/src/lib.rs
+++ concave/src/lib.rs
@@ -1,7 +1,6 @@
 use geo::{ConcaveHull, MultiPoint};
 
 use pyo3::prelude::*;
-// use pyo3::wrap_pyfunction;
 
 type Points = Vec<[f64; 2]>;
 
@@ -18,13 +17,13 @@ fn rconcave(_py: Python, m: &PyModule) -> PyResult<()> {
     ///
     /// returns points: Vec<f64>[m, 2]
     #[pyfn(m)]
-    #[text_signature = "(points, concavity, /)"]
+    #[pyo3(text_signature = "(points, concavity, /)")]
     fn concave_hull(points: Points, concavity: f64 ) -> PyResult<Points> {
         let m_points: MultiPoint<f64> = points.into();
 
         let hull = m_points.concave_hull(concavity);
     
-        let hull_points = hull.exterior().coords().map(|c| [c.x, c.y]).collect();
+        let hull_points = hull.exterior().coords().skip(1).map(|c| [c.x, c.y]).collect();
         Ok(hull_points)
     }
     Ok(())
