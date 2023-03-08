58eff020d41dfafef5c20569a714464b4e476d85
GlennWSo <sondellglenn@gmail.com>
2023-03-08 14:36:01 +0100

    updagte concave example

--- examples/concave.py
+++ examples/concave.py
@@ -1,25 +1,32 @@
 import numpy as np
 import pyvista as pv
-
-
 from rscad.concave.rconcave import concave_hull
-
-
-randp = np.random.rand(10, 2)
 
 
 def pad(arr):
     return np.pad(arr, ((0, 0), (0, 1)))
 
 
-rand_mesh = pv.PolyData(pad(randp))
-rand_mesh.plot()
+def random_concave(factor):
 
-concave_points = concave_hull(randp, 2.0)
-n = len(concave_points)
+    # generate random points
+    randp = np.random.rand(50, 2)
+    rand_mesh = pv.PolyData(pad(randp))
+    # rand_mesh.plot() #  inspect input
 
-lines = [n] + [i for i in range(n)]
-print(concave_points[0], concave_points[-1])
+    # find the concave path around the points and create a polygon
+    concave_points = concave_hull(randp, factor)
+    n = len(concave_points)
+    lines = [n] + [i for i in range(n)] + [2, n - 1, 0]
+    polygon = pv.PolyData(pad(concave_points), lines=lines)
 
-con_mesh = pv.PolyData(pad(concave_points), lines)
-con_mesh.plot()
+    # inpect input and result
+    p = pv.Plotter()
+    p.add_mesh(rand_mesh, color="green")
+    p.add_mesh(polygon, color="blue")
+    p.show()
+
+
+if __name__ == "__main__":
+    for _ in range(10):
+        random_concave(2.0)
